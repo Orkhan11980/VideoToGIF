fb4d0dc7fd73ef58b0fac02b3c9af714518957fa
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-10-11 10:54:43 +0400

    Completed

--- videotoGIF.py
+++ videotoGIF.py
@@ -1,5 +1,19 @@
+import os
+
 from moviepy.editor import VideoFileClip
 
-videoClip = VideoFileClip("vide.mp4")
+input_directory = "/Users/macbook/Desktop/videos"
 
-videoClip.write_gif("video.gif")
+output_directory = "/Users/macbook/Desktop/GIFS"
+
+video_files = [f for f in os.listdir(input_directory) if f.endswith(".mp4")]
+
+for filename in video_files:
+    input_video_path = os.path.join(input_directory, filename)
+    output_gif_path = os.path.join(output_directory, os.path.splitext(filename)[0] + ".gif")
+
+video_clip = VideoFileClip(input_video_path)
+video_clip.write_gif(output_gif_path)
+video_clip.close()
+
+print(f"Converted {input_video_path} to {output_gif_path}")
