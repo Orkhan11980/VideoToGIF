f26e2f88d56c97eaf0b83995fb0f9f9b22b15344
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-10-11 12:31:25 +0400

    Completed

--- videotoGIF.py
+++ videotoGIF.py
@@ -2,18 +2,18 @@ import os
 
 from moviepy.editor import VideoFileClip
 
-input_directory = "/path/to/videos"
+input_directory = "Path/to/videos"
 
-output_directory = "/path/to/gifs"
+output_directory = "Path/to/GIFS"
 
 video_files = [f for f in os.listdir(input_directory) if f.endswith(".mp4")]
 
 for filename in video_files:
     input_video_path = os.path.join(input_directory, filename)
     output_gif_path = os.path.join(output_directory, os.path.splitext(filename)[0] + ".gif")
+    video_clip = VideoFileClip(input_video_path)
+    video_clip.write_gif(output_gif_path)
 
-video_clip = VideoFileClip(input_video_path)
-video_clip.write_gif(output_gif_path)
 video_clip.close()
 
 print(f"Converted {input_video_path} to {output_gif_path}")
